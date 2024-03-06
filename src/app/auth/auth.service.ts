082e2c3955353b2bb26f95d8d4e9dd9e96349788
Andrew Post <drdrewusaf@hotmail.com>
2024-03-06 17:03:19 +0100

    Mid update.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,6 +5,8 @@ import { BehaviorSubject, throwError } from "rxjs";
 import { User } from "./user.model";
 import { Router } from "@angular/router";
 
+import { environment } from "../../environments/environment";
+
 
 export interface AuthResponseData {
     kind: string;
@@ -29,7 +31,7 @@ export class AuthService {
 
     signup(email: string, password: string) {
         return this.http.post<AuthResponseData>(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAhpMYkuqkXEVzPBYYwyc2eUw_xW9xeTzk',
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey,
             {
                 email: email,
                 password: password,
@@ -50,7 +52,7 @@ export class AuthService {
 
     login(email: string, password: string) {
         return this.http.post<AuthResponseData>(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAhpMYkuqkXEVzPBYYwyc2eUw_xW9xeTzk',
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey,
             {
                 email: email,
                 password: password,
